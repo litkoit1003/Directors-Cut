488f668c2a546443c64551b32046b1de0e1edee3
litkoit1003 <danakuckanov@gmail.com>
2025-09-13 20:57:01 +0300

    Init commit

---
+++ src/main/java/org/litkoit/directorscut/Directorscut.java
@@ -0,0 +1,10 @@
+package org.litkoit.directorscut;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Directorscut implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
